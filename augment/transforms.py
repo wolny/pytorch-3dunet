36341ff6f575408072365d44f965f5ff3409ce20
lorenzocerrone <lorenzo.cerrone@iwr.uni-heidelberg.de>
2019-01-17 14:40:56 +0100

    fix bug in seg2boundary transformer

--- augment/transforms.py
+++ augment/transforms.py
@@ -470,7 +470,7 @@ class LabelToBoundaryTransformer(BaseTransformer):
                 # removed in favour of Segmentation2Pmap3D
                 # LabelToBoundary(axes=(0, 1, 2), offsets=(1, 4), ignore_index=self.ignore_index),
                 # this will give us 6 output channels with boundary signal
-                ToTensor(expand_dims=False, dtype=self.label_dtype)
+                ToTensor(expand_dims=False, dtype=self.label_dtype),
                 Segmentation2Pmap3D(offset=self.offset)
             ])
 
