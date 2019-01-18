5a2dc13a0cf1018ba11cf447da1e4ad1933c558b
Adrian Wolny <adrian.wolny@iwr.uni-heidelberg.de>
2019-01-18 09:21:36 +0100

    Remove code duplication in various dice losses

--- unet3d/losses.py
+++ unet3d/losses.py
@@ -6,6 +6,36 @@ from torch.autograd import Variable
 SUPPORTED_LOSSES = ['ce', 'bce', 'wce', 'pce', 'dice', 'gdl']
 
 
+def compute_per_channel_dice(input, target, epsilon=1e-5, ignore_index=None, weight=None):
+    # assumes that input is a normalized probability
+
+    # input and target shapes must match
+    if target.dim() == 4:
+        target = expand_as_one_hot(target, C=input.size()[1], ignore_index=ignore_index)
+
+    assert input.size() == target.size(), "'input' and 'target' must have the same shape"
+
+    # mask ignore_index if present
+    if ignore_index is not None:
+        mask = target.clone().ne_(ignore_index)
+        mask.requires_grad = False
+
+        input = input * mask
+        target = target * mask
+
+    input = flatten(input)
+    target = flatten(target)
+
+    target = target.float()
+    # Compute per channel Dice Coefficient
+    intersect = (input * target).sum(-1)
+    if weight is not None:
+        intersect = weight * intersect
+
+    denominator = (input + target).sum(-1)
+    return 2. * intersect / denominator.clamp(min=epsilon)
+
+
 class DiceCoefficient:
     """Computes Dice Coefficient.
     Generalized to multiple channels by computing per-channel Dice Score
@@ -18,35 +48,13 @@ class DiceCoefficient:
         self.ignore_index = ignore_index
 
     def __call__(self, input, target):
-        # assumes that input is already normalized probability
-
-        # input and target shapes must match
-        if target.dim() == 4:
-            target = expand_as_one_hot(target, C=input.size()[1], ignore_index=self.ignore_index)
-
-        assert input.size() == target.size(), "'input' and 'target' must have the same shape"
-
-        # mask ignore_index if present
-        if self.ignore_index is not None:
-            mask = target.clone().ne_(self.ignore_index)
-            mask.requires_grad = False
-
-            input = input * mask
-            target = target * mask
-
-        input = flatten(input)
-        target = flatten(target)
-
-        # Compute per channel Dice Coefficient
-        intersect = (input * target).sum(-1)
-        denominator = (input + target).sum(-1)
         # Average across channels in order to get the final score
-        return torch.mean(2. * intersect / denominator.clamp(min=self.epsilon))
+        return torch.mean(compute_per_channel_dice(input, target, epsilon=self.epsilon, ignore_index=self.ignore_index))
 
 
 class DiceLoss(nn.Module):
-    """Computes Dice Loss. Follows the implementation of the above DiceCoefficient (DSC),
-    but additionally allows per-class weights to be provided.
+    """Computes Dice Loss, which just 1 - DiceCoefficient described above.
+    Additionally allows per-class weights to be provided.
     """
 
     def __init__(self, epsilon=1e-5, weight=None, ignore_index=None, sigmoid_normalization=True):
@@ -67,37 +75,20 @@ class DiceLoss(nn.Module):
     def forward(self, input, target):
         # get probabilities from logits
         input = self.normalization(input)
-        # input and target shapes must match
-        if target.dim() == 4:
-            target = expand_as_one_hot(target, C=input.size()[1], ignore_index=self.ignore_index)
-
-        assert input.size() == target.size(), "'input' and 'target' must have the same shape"
-
-        # mask ignore_index if present
-        if self.ignore_index is not None:
-            mask = target.clone().ne_(self.ignore_index)
-            mask.requires_grad = False
-
-            input = input * mask
-            target = target * mask
-
-        input = flatten(input)
-        target = flatten(target)
-
-        # Compute per channel Dice Coefficient
-        intersect = (input * target).sum(-1)
         if self.weight is not None:
             weight = Variable(self.weight, requires_grad=False)
-            intersect = weight * intersect
+        else:
+            weight = None
 
-        denominator = (input + target).sum(-1)
+        per_channel_dice = compute_per_channel_dice(input, target, epsilon=self.epsilon, ignore_index=self.ignore_index,
+                                                    weight=weight)
         # Average the Dice score across all channels/classes
-        return torch.mean(1. - 2. * intersect / denominator.clamp(min=self.epsilon))
+        return torch.mean(1. - per_channel_dice)
 
 
 class GeneralizedDiceLoss(nn.Module):
     """Computes Generalized Dice Loss (GDL) as described in https://arxiv.org/pdf/1707.03237.pdf
-    Combines a `Softmax` layer with the GDL, so it expects logits as input.
+    TODO: double-check the equations in the paper and the current implementation
     """
 
     def __init__(self, epsilon=1e-5, weight=None, ignore_index=None, sigmoid_normalization=True):
@@ -113,34 +104,19 @@ class GeneralizedDiceLoss(nn.Module):
     def forward(self, input, target):
         # get probabilities from logits
         input = self.normalization(input)
-        # input and target shapes must match
-        if target.dim() == 4:
-            target = expand_as_one_hot(target, C=input.size()[1], ignore_index=self.ignore_index)
-
-        assert input.size() == target.size(), "'input' and 'target' must have the same shape"
-
-        # mask ignore_index if present
-        if self.ignore_index is not None:
-            mask = target.clone().ne_(self.ignore_index)
-            mask.requires_grad = False
-
-            input = input * mask
-            target = target * mask
-
-        input = flatten(input)
-        target = flatten(target)
-
+        target = target.float()
         target_sum = target.sum(-1)
         class_weights = 1. / (target_sum * target_sum).clamp(min=self.epsilon)
 
-        intersect = (input * target).sum(-1) * class_weights
         if self.weight is not None:
-            weight = Variable(self.weight, requires_grad=False)
-            intersect = weight * intersect
+            weight = Variable(self.weight * class_weights, requires_grad=False)
+        else:
+            weight = Variable(class_weights, requires_grad=False)
 
-        denominator = (input + target).sum(-1) * class_weights
-
-        return 1. - 2. * intersect.sum() / denominator.sum().clamp(min=self.epsilon)
+        per_channel_dice = compute_per_channel_dice(input, target, epsilon=self.epsilon, ignore_index=self.ignore_index,
+                                                    weight=weight)
+        # Average the Dice score across all channels/classes
+        return torch.mean(1. - per_channel_dice)
 
 
 class WeightedCrossEntropyLoss(nn.Module):
