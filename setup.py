84cd6bf3536fa8f199d3431c3f2b5a671be6c224
Adrian Wolny <adrian.wolny@iwr.uni-heidelberg.de>
2020-01-16 16:00:57 +0100

    Update meta.yaml

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="pytorch3dunet",
     packages=find_packages(exclude=["tests"]),
-    version="1.0.0",
+    version="1.0.1",
     author="Adrian Wolny, Lorenzo Cerrone",
     url="https://github.com/wolny/pytorch-3dunet",
     license="MIT",
+    install_requires=['tensorboardx'],
     python_requires='>=3.6'
 )
