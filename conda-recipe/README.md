b577b5c558e800f31e5ff73a67f05d576c3482b3
Adrian Wolny <adrian.wolny@iwr.uni-heidelberg.de>
2020-01-16 14:43:46 +0100

    Create conda-recipe

---
+++ conda-recipe/README.md
@@ -0,0 +1,3 @@
+## Conda build
+In order to create conda package:
+1. Run: `conda build conda-recipe/meta.yaml -c conda-forge`
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pytorch3dunet",
+    packages=find_packages(exclude=["tests"]),
+    version="1.0.0",
+    author="Adrian Wolny, Lorenzo Cerrone",
+    url="https://github.com/wolny/pytorch-3dunet",
+    license="MIT",
+    python_requires='>=3.6'
+)
