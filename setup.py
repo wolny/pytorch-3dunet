0cdf4dafec3a3854a1d3dbd2aa266f0f0d214281
Adrian Wolny <adrian.wolny@iwr.uni-heidelberg.de>
2020-01-16 17:29:56 +0100

    Update conda-recipe

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="pytorch3dunet",
     packages=find_packages(exclude=["tests"]),
-    version="1.0.1",
+    version="1.0.2",
     author="Adrian Wolny, Lorenzo Cerrone",
     url="https://github.com/wolny/pytorch-3dunet",
     license="MIT",
-    python_requires='>=3.6'
+    python_requires='>=3.7'
 )
