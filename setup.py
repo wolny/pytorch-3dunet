19b993a1c00eec676bc2bcd38c2695124b2a2beb
Adrian Wolny <adrian.wolny@iwr.uni-heidelberg.de>
2020-01-17 13:46:13 +0100

    Minor changes in the setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
 
+_version_ = "1.0.2"
+
 setup(
     name="pytorch3dunet",
     packages=find_packages(exclude=["tests"]),
-    version="1.0.2",
+    version=_version_,
     author="Adrian Wolny, Lorenzo Cerrone",
     url="https://github.com/wolny/pytorch-3dunet",
     license="MIT",
