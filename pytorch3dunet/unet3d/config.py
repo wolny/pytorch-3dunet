1bb82a30623f17c3103e23e6f7d31568c60eeb84
Adrian Wolny <adrian.wolny@iwr.uni-heidelberg.de>
2020-02-03 21:50:11 +0100

    Fix device configuration

--- pytorch3dunet/unet3d/config.py
+++ pytorch3dunet/unet3d/config.py
@@ -3,14 +3,28 @@ import argparse
 import torch
 import yaml
 
+from pytorch3dunet.unet3d import utils
+
+logger = utils.get_logger('ConfigLoader')
+
 
 def load_config():
-    parser = argparse.ArgumentParser(description='UNet3D training')
+    parser = argparse.ArgumentParser(description='UNet3D')
     parser.add_argument('--config', type=str, help='Path to the YAML config file', required=True)
     args = parser.parse_args()
     config = _load_config_yaml(args.config)
     # Get a device to train on
-    device = torch.device("cuda:0" if torch.cuda.is_available() else 'cpu')
+    device_str = config.get('device', None)
+    if device_str is not None:
+        logger.info(f"Device specified in config: '{device_str}'")
+        if device_str.startswith('cuda') and not torch.cuda.is_available():
+            logger.warn('CUDA not available, using CPU')
+            device_str = 'cpu'
+    else:
+        device_str = "cuda:0" if torch.cuda.is_available() else 'cpu'
+        logger.info(f"Using '{device_str}' device")
+
+    device = torch.device(device_str)
     config['device'] = device
     return config
 
