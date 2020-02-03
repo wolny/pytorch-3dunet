21acc85e7be8d5fcee083f579c81f076977527b5
Adrian Wolny <adrian.wolny@iwr.uni-heidelberg.de>
2020-02-03 23:32:36 +0100

    Fix duplicate logging

--- pytorch3dunet/unet3d/utils.py
+++ pytorch3dunet/unet3d/utils.py
@@ -80,17 +80,26 @@ def save_network_output(output_path, output, logger=None):
         f.create_dataset('predictions', data=output, compression='gzip')
 
 
-def get_logger(name, level=logging.INFO):
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
-    # Logging to console
-    stream_handler = logging.StreamHandler(sys.stdout)
-    formatter = logging.Formatter(
-        '%(asctime)s [%(threadName)s] %(levelname)s %(name)s - %(message)s')
-    stream_handler.setFormatter(formatter)
-    logger.addHandler(stream_handler)
+loggers = {}
 
-    return logger
+
+def get_logger(name, level=logging.INFO):
+    global loggers
+    if loggers.get(name) is not None:
+        return loggers[name]
+    else:
+        logger = logging.getLogger(name)
+        logger.setLevel(level)
+        # Logging to console
+        stream_handler = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter(
+            '%(asctime)s [%(threadName)s] %(levelname)s %(name)s - %(message)s')
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
+
+        loggers[name] = logger
+
+        return logger
 
 
 def get_number_of_learnable_parameters(model):
