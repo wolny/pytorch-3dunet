9d103b83bb9e0b31e498b730a921972da1224b4a
Qin Yu <qin.yu.95@outlook.com>
2022-11-25 10:18:40 +0100

    Fix duplicate label (ground truth) checking and docs
    AdaptedRandError compute np.unique twice, and has the wrong dimension in docstring.

--- pytorch3dunet/unet3d/metrics.py
+++ pytorch3dunet/unet3d/metrics.py
@@ -126,18 +126,12 @@ class AdaptedRandError:
         Compute ARand Error for each input, target pair in the batch and return the mean value.
 
         Args:
-            input (torch.tensor): 5D (NCDHW) output from the network
-            target (torch.tensor): 4D (NDHW) ground truth segmentation
+            input (torch.tensor):  5D (NCDHW) output from the network
+            target (torch.tensor): 5D (NCDHW) ground truth segmentation
 
         Returns:
             average ARand Error across the batch
         """
-
-        def _arand_err(gt, seg):
-            n_seg = len(np.unique(seg))
-            if n_seg == 1:
-                return 0.
-            return adapted_rand_error(gt, seg)[0]
 
         # converts input and target to numpy arrays
         input, target = convert_to_numpy(input, target)
@@ -169,7 +163,7 @@ class AdaptedRandError:
             assert segm.ndim == 4
 
             # compute per channel arand and return the minimum value
-            per_channel_arand = [_arand_err(_target, channel_segm) for channel_segm in segm]
+            per_channel_arand = [adapted_rand_error(_target, channel_segm)[0] for channel_segm in segm]
             per_batch_arand.append(np.min(per_channel_arand))
 
         # return mean arand error
